913a222902f913e06f828494630ea948964194ba
linzhiqiang <1913045515@qq.com>
2018-08-07 19:33:26 +0800

    add project

--- SpringBoot/SpringBootLog4j2/src/main/java/com/example/demo/controller/Log4j2Controller.java
+++ SpringBoot/SpringBootLog4j2/src/main/java/com/example/demo/controller/Log4j2Controller.java
@@ -1,4 +1,9 @@
 package com.example.demo.controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author linzhiqiang
